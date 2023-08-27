b615fd28d1ee0dd86ca86aaf6ad22fbd397c1873
yumemiso2 <84170344+yumemiso2@users.noreply.github.com>
2023-08-27 20:09:09 +0900

    feat: swiper導入、スライドに対して中央揃えになるようにロゴと紹介文を配置

---
+++ js/index.js
@@ -0,0 +1,23 @@
+//ELは必須の設定
+
+const swiper = new Swiper('.swiper', {
+  loop: false,//スライドを繰り返しする
+
+  navigation: {
+    nextEl: '.swiper-button-next',//次へボタン
+    prevEl: '.swiper-button-prev',//前へボタン
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',//スクロールバー
+  },
+
+// https://codesandbox.io/p/sandbox/vf5lq2?file=/index.html:52,1
+  slidesPerView: "auto",//スライダーコンテナにスライドを-枚同時表示
+  centeredSlides: true,//アクティブなスライドを中央に表示
+  spaceBetween: 30,//スライド間の距離
+  pagination: {
+    el: ".swiper-pagination",//スライドの枚数と現在アクティブなスライドがわかる
+    clickable: true,
+  }
+});
