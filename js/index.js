19ad8dab966599a688847a34c1c0df5c49f29ee2
yumemiso2 <84170344+yumemiso2@users.noreply.github.com>
2023-08-28 20:45:37 +0900

    feat: hoverアクションを追加・フォント適用など

--- js/index.js
+++ js/index.js
@@ -21,3 +21,21 @@ const swiper = new Swiper('.swiper', {
     clickable: true,
   }
 });
+
+
+// swiper のスライドをキーボードで操作する
+swiper.keyboard.enable();   // ←これを追加
+
+// let previndex = swiper.activeIndex;
+// // swiper の最後のスライドのインデックスを取得する
+// const lastIndex = swiper.slides.length - 1;
+
+// swiper.on('keyPress', function (_, keyCode) {
+//   // アクティブなスライドが最後のスライドだったら
+//   if (previndex === lastIndex && keyCode === 39) {
+//     // 最初のスライドに戻る
+//     swiper.slideTo(0);
+//   }
+
+//   previndex = swiper.activeIndex;
+// } );
