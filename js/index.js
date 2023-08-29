97333f371fad787e5b35a25ecdebce5707e3a640
yumemiso2 <84170344+yumemiso2@users.noreply.github.com>
2023-08-29 11:29:30 +0900

    refactor: プルリク修正、スクロールバーを消すところまで

--- js/index.js
+++ js/index.js
@@ -8,9 +8,6 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',//前へボタン
   },
 
-  scrollbar: {
-    el: '.swiper-scrollbar',//スクロールバー
-  },
 
 // https://codesandbox.io/p/sandbox/vf5lq2?file=/index.html:52,1
   slidesPerView: "auto",//スライダーコンテナにスライドを-枚同時表示
