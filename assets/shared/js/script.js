af0f0587a166c5e14660fba12a26120960d640a0
yumemiso2 <84170344+yumemiso2@users.noreply.github.com>
2023-12-19 16:51:06 +0900

    見出し・改行の修正など

--- assets/shared/js/script.js
+++ assets/shared/js/script.js
@@ -1,34 +1,7 @@
-// const scrollCount = window.scrollY;
-// const scrollFadein = document.querySelectorAll(".js-scrollfadein");
-
-// const fadeInEffect = (entries, obs) => {
-//   entries.forEach((entry) => {
-//     // もし画面内に監視対象が入っているなら、
-//     if (entry.isIntersecting){
-//       // console.log(entry.target);
-//       entry.target.animate (
-//         {
-//           opacity: [0,1],
-//           filter: ["blur(.4rem)", "blur(0)"],
-//           translate:["0 4rem", 0],
-//         },
-//         {
-//           duration: 2000,
-//           easing: "ease",
-//           fill: "forwards",
-//         }
-//       );
-//       obs.unobserve(entry.target);
-//     }
-//   });
-//   console.log("ふわり");
-// }
-
 const fadeInEffect = (entries, obs) => {
   entries.forEach((entry) => {
     // もし画面内に監視対象が入っているなら、
     if (entry.isIntersecting){
-      // console.log(entry.target);
       entry.target.animate (
         {
           opacity: [0,1],
@@ -44,12 +17,10 @@ const fadeInEffect = (entries, obs) => {
       obs.unobserve(entry.target);
     }
   });
-  console.log("ふわり");
 }
 
 const fadeObserver = new IntersectionObserver(fadeInEffect);
 const fadeInContents = document.querySelectorAll(".js-scrollfadein");
-// console.log(fadeInContents);
 
 fadeInContents.forEach((fadeInContent) => {
   fadeObserver.observe(fadeInContent);
