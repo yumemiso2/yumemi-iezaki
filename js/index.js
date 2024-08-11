529636675e4929ba71a3cde18ca2cdd3a797df9e
Hiroki Kajiya <kajiya.hiroki@gmail.com>
2024-08-11 20:04:53 +0900

    モーダル内のスライドの調整

--- js/index.js
+++ js/index.js
@@ -23,52 +23,20 @@ const swiper = new Swiper('#HomeWorks__swiper', {
   }
 });
 
-// ModalItemImageSwiper　の初期化をする（モーダル内で複数回使うので、クラス名を使用する）
-window.onload = () => {
-  const ModalItemImageSwiper = new Swiper('.HomePracticeModalImg__swiper', {
-    // loop: false,//スライドを繰り返しする
-    loop: true,
-    speed: 2000,  //スライド切り替えアニメーションのスピードを指定
-    autoplay: {
-      delay: 1000,  //次のスライドに切り替わるまでの時間
-      disableOnInteraction: false,  // ユーザーがページを触っても自動再生を続行する
-    },
+const modalImgList = document.querySelectorAll('.HomePracticeModalImg__swiper');
+const modalImgSwiper = [];
 
-    // https://codesandbox.io/p/sandbox/vf5lq2?file=/index.html:52,1
-    // slidesPerView: "auto",//スライダーコンテナにスライドを-枚同時表示
+modalImgList.forEach((modalImg, i) => {
+  modalImgSwiper[i] = new Swiper(modalImg, {
+    loop: true,
+    speed: 2000, //スライド切り替えアニメーションのスピードを指定
+    allowTouchMove: false, // ユーザー操作を無効にする
     centeredSlides: true,//アクティブなスライドを中央に表示
     pagination: {
-      el: ".HomePracticeModalImg__swiper-pagination",//スライドの枚数と現在アクティブなスライドがわかる
-      // clickable: true,
+      el: modalImg.querySelector(".HomePracticeModalImg__swiper-pagination"), //スライドの枚数と現在アクティブなスライドがわかる
     },
   });
-  ModalItemImageSwiper.autoplay.stop();
-
-  window.addEventListener("click", () => {
-    const ModalImgMove = document.querySelector(".HomePracticeModalImg__swiperMove");
-    if (ModalImgMove) {
-      ModalImgMove.addEventListener("click", () => {
-        ModalItemImageSwiper.autoplay.start();
-      });
-    } else {
-      ModalItemImageSwiper.autoplay.stop();
-    }
-  });
-  // const ModalImgMove = document.querySelector(".HomePracticeModalImg__swiperMove");
-  // ModalImgMove.addEventListener("click", ModalItemImageSwiper.autoplay.start());
-
-}
-
-
-
-// addEventListener("load",() => {
-//   const ModalImgMove = document.querySelector(".HomePracticeModalImg__swiperMove");
-//   if(ModalImgMove.onload){
-//     ModalItemImageSwiper.autoplay.start();
-//   }
-//   console.log(ModalImgMove);
-//   // const HomePracticeModalImg__swiperMove =
-// })
+});
 
 // ModalSwiper の初期化
 const MicroModalSwiper = new Swiper('#HomePractice__swiper', {
@@ -103,6 +71,11 @@ scrollListItemLink.forEach(function (link, i) {
     function () {
       // console.log(i + " をクリックした!!");
       MicroModalSwiper.slideTo(i, 0);
+      
+      modalImgSwiper.forEach(swiper => {
+        swiper.slideTo(0, 0); // スライドを1枚目に戻す
+        swiper.autoplay.start(); // 再生を開始する
+      });
     }
   );
 });
