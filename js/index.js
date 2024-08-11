3f6071fb4b2147d1f8503642094be1a1c0d25079
yumemiso2 <84170344+yumemiso2@users.noreply.github.com>
2024-08-11 23:25:28 +0900

    change: モーダル内画像のスライドがループしない問題を解決？

--- js/index.js
+++ js/index.js
@@ -24,87 +24,61 @@ const swiper = new Swiper('#HomeWorks__swiper', {
 });
 
 // ModalItemImageSwiper　の初期化をする（モーダル内で複数回使うので、クラス名を使用する）
-window.onload = () => {
-  const ModalItemImageSwiper = new Swiper('.HomePracticeModalImg__swiper', {
-    // loop: false,//スライドを繰り返しする
+const modalImgList = document.querySelectorAll(".HomePracticeModalImg__swiper");
+const modalImgSwiper = [];
+
+modalImgList.forEach((modalImg, i) => {
+  modalImgSwiper[i] = new Swiper(modalImg, {
     loop: true,
-    speed: 2000,  //スライド切り替えアニメーションのスピードを指定
+    speed: 2000,//スライド切り替えアニメーションのスピードを指定
+    // allowTouchMove: false, // ユーザー操作を無効にする
     autoplay: {
-      delay: 1000,  //次のスライドに切り替わるまでの時間
       disableOnInteraction: false,  // ユーザーがページを触っても自動再生を続行する
     },
-
-    // https://codesandbox.io/p/sandbox/vf5lq2?file=/index.html:52,1
-    // slidesPerView: "auto",//スライダーコンテナにスライドを-枚同時表示
     centeredSlides: true,//アクティブなスライドを中央に表示
     pagination: {
-      el: ".HomePracticeModalImg__swiper-pagination",//スライドの枚数と現在アクティブなスライドがわかる
-      // clickable: true,
+      clickable:true,
+      el: modalImg.querySelector(".HomePracticeModalImg__swiper-pagination"),  //スライドの枚数と現在アクティブなスライドがわかる
     },
   });
-  ModalItemImageSwiper.autoplay.stop();
-
-  window.addEventListener("click", () => {
-    const ModalImgMove = document.querySelector(".HomePracticeModalImg__swiperMove");
-    if (ModalImgMove) {
-      ModalImgMove.addEventListener("click", () => {
-        ModalItemImageSwiper.autoplay.start();
-      });
-    } else {
-      ModalItemImageSwiper.autoplay.stop();
-    }
+});
+  // ModalSwiper の初期化
+  const MicroModalSwiper = new Swiper('#HomePractice__swiper', {
+    effect: "fade",
+    autoHeight: false,
+    navigation: {
+      nextEl: "#HomePractice__swiper-button-next",
+      prevEl: "#HomePractice__swiper-button-prev",
+    },
+    pagination: {
+      // el: "#HomePractice__swiper-pagination",
+      clickable: true,
+    },
   });
-  // const ModalImgMove = document.querySelector(".HomePracticeModalImg__swiperMove");
-  // ModalImgMove.addEventListener("click", ModalItemImageSwiper.autoplay.start());
-
-}
 
 
-
-// addEventListener("load",() => {
-//   const ModalImgMove = document.querySelector(".HomePracticeModalImg__swiperMove");
-//   if(ModalImgMove.onload){
-//     ModalItemImageSwiper.autoplay.start();
-//   }
-//   console.log(ModalImgMove);
-//   // const HomePracticeModalImg__swiperMove =
-// })
-
-// ModalSwiper の初期化
-const MicroModalSwiper = new Swiper('#HomePractice__swiper', {
-  effect: "fade",
-  autoHeight: false,
-  navigation: {
-    nextEl: "#HomePractice__swiper-button-next",
-    prevEl: "#HomePractice__swiper-button-prev",
-  },
-  pagination: {
-    // el: "#HomePractice__swiper-pagination",
-    clickable: true,
-  },
-});
+  // MicroModalの初期化
+  MicroModal.init({ disableScroll: true });
 
 
-
-// MicroModalの初期化
-MicroModal.init({ disableScroll: true });
-
-
-// クリックしたれんしゅう作品のサムネイルと、モーダルのスライドページが対応するようにした
-const scrollListItemLink = document.querySelectorAll(".js-modal");
+  // クリックしたれんしゅう作品のサムネイルと、モーダルのスライドページが対応するようにした
+  const scrollListItemLink = document.querySelectorAll(".js-modal");
 
 
-// モーダルのインデックスを切り替える
-// forEach は scrollListItemLink の中身を一個づつ取り出す処理
-scrollListItemLink.forEach(function (link, i) {
-  // console.log(i + ' 回目の処理', link, scrollListItemLink[i]);
-  link.addEventListener(
-    "click",
-    function () {
-      // console.log(i + " をクリックした!!");
-      MicroModalSwiper.slideTo(i, 0);
-    }
-  );
-});
+  // モーダルのインデックスを切り替える
+  // forEach は scrollListItemLink の中身を一個づつ取り出す処理
+  scrollListItemLink.forEach(function (link, i) {
+    // console.log(i + ' 回目の処理', link, scrollListItemLink[i]);
+    link.addEventListener(
+      "click",
+      function () {
+        // console.log(i + " をクリックした!!");
+        MicroModalSwiper.slideTo(i, 0);
 
-// scrollListItemLink();
+        modalImgSwiper.forEach(swiper => {
+          swiper.slideTo(0, 0); // スライドを1枚目に戻す
+          swiper.autoplay.start(); // 再生を開始する
+        });
+      }
+    );
+  });
