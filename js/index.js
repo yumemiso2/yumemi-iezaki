77a7b151037037c28a5a84a8a45ba8a7e5ec3b10
yumemiso2 <84170344+yumemiso2@users.noreply.github.com>
2024-08-11 18:55:08 +0900

    pullrequest: モーダル内画像のSwiper、モーダルの開閉やモーダルの別ページを参照して戻ってくると、自動スクロールが機能しなくなる問題の相談

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ const swiper = new Swiper('#HomeWorks__swiper', {
   },
 
 
-// https://codesandbox.io/p/sandbox/vf5lq2?file=/index.html:52,1
+  // https://codesandbox.io/p/sandbox/vf5lq2?file=/index.html:52,1
   slidesPerView: "auto",//スライダーコンテナにスライドを-枚同時表示
   centeredSlides: true,//アクティブなスライドを中央に表示
   loop: true,
@@ -24,33 +24,54 @@ const swiper = new Swiper('#HomeWorks__swiper', {
 });
 
 // ModalItemImageSwiper　の初期化をする（モーダル内で複数回使うので、クラス名を使用する）
-const ModalItemImageSwiper = new Swiper('.HomePracticeModalImg__swiper', {
-  loop: false,//スライドを繰り返しする
-  // loop: true,
-  keyboard: {
-    enabled: true,
-  },
+window.onload = () => {
+  const ModalItemImageSwiper = new Swiper('.HomePracticeModalImg__swiper', {
+    // loop: false,//スライドを繰り返しする
+    loop: true,
+    speed: 2000,  //スライド切り替えアニメーションのスピードを指定
+    autoplay: {
+      delay: 1000,  //次のスライドに切り替わるまでの時間
+      disableOnInteraction: false,  // ユーザーがページを触っても自動再生を続行する
+    },
 
-  // navigation: {
-  //   nextEl: '#HomePracticeModalImg__swiper-button-next',//次へボタン
-  //   prevEl: '#HomePracticeModalImg__swiper-button-prev',//前へボタン
-  // },
+    // https://codesandbox.io/p/sandbox/vf5lq2?file=/index.html:52,1
+    // slidesPerView: "auto",//スライダーコンテナにスライドを-枚同時表示
+    centeredSlides: true,//アクティブなスライドを中央に表示
+    pagination: {
+      el: ".HomePracticeModalImg__swiper-pagination",//スライドの枚数と現在アクティブなスライドがわかる
+      // clickable: true,
+    },
+  });
+  ModalItemImageSwiper.autoplay.stop();
+
+  window.addEventListener("click", () => {
+    const ModalImgMove = document.querySelector(".HomePracticeModalImg__swiperMove");
+    if (ModalImgMove) {
+      ModalImgMove.addEventListener("click", () => {
+        ModalItemImageSwiper.autoplay.start();
+      });
+    } else {
+      ModalItemImageSwiper.autoplay.stop();
+    }
+  });
+  // const ModalImgMove = document.querySelector(".HomePracticeModalImg__swiperMove");
+  // ModalImgMove.addEventListener("click", ModalItemImageSwiper.autoplay.start());
+
+}
 
 
-// https://codesandbox.io/p/sandbox/vf5lq2?file=/index.html:52,1
-  // slidesPerView: "auto",//スライダーコンテナにスライドを-枚同時表示
-  centeredSlides: true,//アクティブなスライドを中央に表示
-  loop: true,
-  pagination: {
-    el: ".HomePracticeModalImg__swiper-pagination",//スライドの枚数と現在アクティブなスライドがわかる
-    clickable: true,
-  }
-});
 
-
+// addEventListener("load",() => {
+//   const ModalImgMove = document.querySelector(".HomePracticeModalImg__swiperMove");
+//   if(ModalImgMove.onload){
+//     ModalItemImageSwiper.autoplay.start();
+//   }
+//   console.log(ModalImgMove);
+//   // const HomePracticeModalImg__swiperMove =
+// })
 
 // ModalSwiper の初期化
-const MicroModalSwiper = new Swiper('#HomePractice__swiper',{
+const MicroModalSwiper = new Swiper('#HomePractice__swiper', {
   effect: "fade",
   autoHeight: false,
   navigation: {
@@ -66,7 +87,7 @@ const MicroModalSwiper = new Swiper('#HomePractice__swiper',{
 
 
 // MicroModalの初期化
-MicroModal.init({disableScroll: true});
+MicroModal.init({ disableScroll: true });
 
 
 // クリックしたれんしゅう作品のサムネイルと、モーダルのスライドページが対応するようにした
@@ -75,11 +96,11 @@ const scrollListItemLink = document.querySelectorAll(".js-modal");
 
 // モーダルのインデックスを切り替える
 // forEach は scrollListItemLink の中身を一個づつ取り出す処理
-scrollListItemLink.forEach(function(link, i){
+scrollListItemLink.forEach(function (link, i) {
   // console.log(i + ' 回目の処理', link, scrollListItemLink[i]);
   link.addEventListener(
     "click",
-    function(){
+    function () {
       // console.log(i + " をクリックした!!");
       MicroModalSwiper.slideTo(i, 0);
     }
